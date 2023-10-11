1d4b925aeeb07719c8621d4322c76f986e51000e
OniDem1 <spaindecraft17@gmail.com>
2023-10-11 13:58:36 +0700

    *

--- TaxiApp/Pages/MainPage.xaml.cs
+++ TaxiApp/Pages/MainPage.xaml.cs
@@ -1,17 +1,33 @@
-﻿namespace TaxiApp
+﻿
+namespace TaxiApp
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        private string _phoneEntry { get; set; }
 
         public MainPage()
         {
             InitializeComponent();
+            PhoneEntry.Text = "";
+        }
+        private bool is_validPhoneNumber()
+        {
+            if (_phoneEntry.Length < 18 )
+            {
+                DisplayAlert("Ошибка ввода", "Неверный номер телефона!", "OK");
+                return false;
+            }
+            return true;
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void Send_Clicked(object sender, EventArgs e)
         {
-            
+            _phoneEntry = PhoneEntry.Text;
+            if(is_validPhoneNumber())
+            {
+                CodeEntry.IsVisible = true;
+            }
+
         }
     }
 }
